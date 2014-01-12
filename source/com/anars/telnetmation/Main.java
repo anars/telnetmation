5df23b4be3b00be603c5843c99d159792546fdf5
Kay Anar <github@anars.com>
2014-01-12 03:49:04 -0500

    first source commit

---
+++ source/com/anars/telnetmation/Main.java
@@ -0,0 +1,160 @@
+/**
+ * Telnetmation - Telnet Based ASCII Animation Player
+ * Copyright (c) 2014 Anar Software LLC. < http://anars.com >
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program.  If not, see < http://www.gnu.org/licenses/ >
+ * 
+ */
+package com.anars.telnetmation;
+
+import java.io.File;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
+import java.util.Calendar;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Main class.
+ *
+ * @author Anar Software LLC
+ * @version 1.0
+ *
+ */
+public class Main
+{
+  private static Logger _logger = Logger.getLogger("com.anars.telnetmation");
+
+  public static void main(String[] args)
+  {
+    int listenPort = 0;
+    InetAddress bindAddress = null;
+    File filePath = null;
+    boolean center = false;
+    for (int index = 0; index < args.length; index++)
+    {
+      String values[] = args[index].split("=");
+      if (values[0].startsWith("-port"))
+      {
+        try
+        {
+          listenPort = Integer.parseInt(values[1]);
+          if (listenPort < 0 || listenPort > 65535)
+            errorExit("-port value must be between 1 and 65535", -1);
+        }
+        catch (Exception exception)
+        {
+          errorExit("Invalid -port value", -2);
+        }
+      }
+      else if (values[0].startsWith("-address"))
+      {
+        try
+        {
+          bindAddress = InetAddress.getByName(values[1]);
+        }
+        catch (Exception exception)
+        {
+          errorExit("Invalid -address value", -3);
+        }
+      }
+      else if (values[0].startsWith("-file"))
+      {
+        try
+        {
+          filePath = new File(values[1]);
+          if (!filePath.exists())
+            errorExit("Invalid -file value, \"" + filePath.toString() + "\" not exists", -4);
+        }
+        catch (Exception exception)
+        {
+          errorExit("Invalid -file value", -5);
+        }
+      }
+      else if (values[0].startsWith("-center"))
+      {
+        values[1] = values[1].toLowerCase();
+        if (values[1].equals("true"))
+          center = true;
+        else if (values[1].equals("false"))
+          center = false;
+        else
+          errorExit("Invalid -center value", -6);
+      }
+      else if (values[0].startsWith("-help"))
+      {
+        System.out.println("\nTelnetmation version 1.0\n" + //
+          "Copyright (c) " + Calendar.getInstance().get(Calendar.YEAR) + " Anar Software LLC. < http://anars.com >\n\n" + //
+          "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\n" + //
+          "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n" + //
+          "See the GNU General Public License for more details at http://www.gnu.org/licenses\n\n" + //
+          "USAGE:\n" + //
+          "\tjava -jar telnetmation.jar [PARAMETERS]...\n\n" + //
+          "MANDATORY PARAMETER:\n\n" + //
+          "-file=[FILE-PATH]\n" + //
+          "\tFull path to ascii animation file.\n" + //
+          "\tE.g. -file=ascii_animation.txt\n\n" + //
+          "OPTIONAL PARAMETERS:\n\n" + //
+          "-port=[NUMBER]\n" + //
+          "\tThe TCP port on which telnetmation will listen. The number must be between 1 and 65535, inclusive. Default, any free local TCP port.\n" + //
+          "\tE.g. -port=8080\n\n" + //
+          "-address=[INET-ADDRESS]\n" + //
+          "\tLocal IP address or hostname which telnetmation will only accept connection requests from. Default, any/all local addresses.\n" + //
+          "\tE.g. -address=192.168.1.100\n\n" + //
+          "-center=[TRUE-FALSE]\n" + //
+          "\tCenters ASCII animation both horizontally and vertically on the terminal screen.\n" + //
+          "\tE.g. -center=true\n");
+        System.exit(0);
+      }
+      else
+        errorExit("Unknown parameter \"" + args[index] + "\"", -7);
+    }
+    if (filePath == null)
+      errorExit("-file parameter is missing", -8);
+
+    ServerSocket serverSocket = null;
+    try
+    {
+      serverSocket = new ServerSocket(listenPort, 0, bindAddress);
+    }
+    catch (Exception exception)
+    {
+      errorExit("Unable to bind socket - " + exception.getMessage(), -9);
+    }
+    _logger.log(Level.INFO, "Telnetmation started.");
+    _logger.log(Level.CONFIG, "Host  : " + serverSocket.getInetAddress().getHostAddress());
+    _logger.log(Level.CONFIG, "Port  : " + serverSocket.getLocalPort());
+    _logger.log(Level.CONFIG, "File  : " + filePath);
+    _logger.log(Level.CONFIG, "Center: " + center);
+    while (true)
+    {
+      try
+      {
+        (new Player(serverSocket.accept(), filePath, center)).start();
+      }
+      catch (Exception exception)
+      {
+        _logger.log(Level.SEVERE, "", exception);
+      }
+    }
+  }
+
+  private static void errorExit(String message, int errorCode)
+  {
+    System.err.println(message + ". Please type -help for details.");
+    System.exit(errorCode);
+  }
+}
---
+++ source/com/anars/telnetmation/Player.java
@@ -0,0 +1,170 @@
+/**
+ * Telnetmation - Telnet Based ASCII Animation Player
+ * Copyright (c) 2014 Anar Software LLC. < http://anars.com >
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program.  If not, see < http://www.gnu.org/licenses/ >
+ * 
+ */
+package com.anars.telnetmation;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
+
+import java.net.Socket;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Player class.
+ *
+ * @author Anar Software LLC
+ * @version 1.0
+ *
+ */
+public class Player
+  extends Thread
+{
+  private static final double VERSION = 1.0;
+  private static final String VT100_INIT = "\u001b[0m";
+  private static final String VT100_HOME = "\u001b[H";
+  private static final String VT100_CLEAR = "\u001b[J";
+  private static Logger _logger = Logger.getLogger("com.anars.telnetmation");
+
+  private Socket _socket;
+  private DataOutputStream _dataOutputStream;
+  private BufferedReader _bufferedReader;
+  private boolean _center;
+
+  Player(Socket socket, File file, boolean center)
+    throws Exception
+  {
+    super();
+    _socket = socket;
+    _center = center;
+    try
+    {
+      _dataOutputStream = new DataOutputStream(socket.getOutputStream());
+      _bufferedReader = new BufferedReader(new FileReader(file));
+      _logger.log(Level.FINE, "Connection from " + socket.getRemoteSocketAddress().toString());
+    }
+    catch (Exception exception)
+    {
+      closeAll();
+      throw new Exception(exception);
+    }
+  }
+
+  public void run()
+  {
+    try
+    {
+      int width = 0;
+      int height = 0;
+      long delayMultiplier = 0;
+      int delay = -1;
+      int leftMargin = 0;
+      int topMargin = 0;
+      String line;
+      int lineCount = 0;
+      String output = VT100_INIT + VT100_HOME + VT100_CLEAR;
+      while ((line = _bufferedReader.readLine()) != null)
+      {
+        if (delayMultiplier == 0)
+        {
+          String[] info = line.split(":");
+          if (Double.parseDouble(info[0]) > VERSION)
+            System.out.println("");
+          width = Integer.parseInt(info[1]);
+          height = Integer.parseInt(info[2]);
+          if (_center)
+          {
+            leftMargin = (80 - width) / 2;
+            topMargin = (24 - height) / 2;
+          }
+          delayMultiplier = Long.parseLong(info[3]);
+        }
+        else
+        {
+          if (delay == -1)
+            delay = Integer.parseInt(line);
+          else
+          {
+            for (int index = 0; !line.equals("") && _center && index < leftMargin; index++)
+              output += " ";
+            output += line;
+            if (++lineCount < height)
+              output += "\n";
+            else
+            {
+              _dataOutputStream.writeBytes(output + VT100_HOME);
+              sleep(delayMultiplier * delay);
+              output = VT100_HOME + VT100_CLEAR;
+              for (int index = 0; _center && index < topMargin; index++)
+                output += "\n";
+              delay = -1;
+              lineCount = 0;
+            }
+          }
+        }
+      }
+      _dataOutputStream.writeBytes(VT100_HOME + VT100_CLEAR);
+    }
+    catch (Throwable throwable)
+    {
+      printError(throwable);
+    }
+    finally
+    {
+      closeAll();
+    }
+  }
+
+  private void closeAll()
+  {
+    try
+    {
+      _bufferedReader.close();
+    }
+    catch (Exception exception)
+    {
+      printError(exception);
+    }
+    _bufferedReader = null;
+    try
+    {
+      _dataOutputStream.close();
+    }
+    catch (Exception exception)
+    {
+      printError(exception);
+    }
+    _dataOutputStream = null;
+    try
+    {
+      _socket.close();
+    }
+    catch (Exception exception)
+    {
+      printError(exception);
+    }
+    _socket = null;
+  }
+
+  private void printError(Throwable throwable)
+  {
+    _logger.log(Level.SEVERE, "", throwable);
+  }
+}
